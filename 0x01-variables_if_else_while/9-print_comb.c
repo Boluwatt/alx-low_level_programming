e290ebe959acf9d851c515085f3fd7a42eee67eb
Boluwatt <69689649+Boluwatt@users.noreply.github.com>
2022-09-09 15:39:09 -0700

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
+ * main - Entry point
  *
- *  *
- *
- *  * Return: Always 0 (Success/correct)
- *
- **/
-
+ * Return: Always 0 (Success/correct)
+ */
 int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)	
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 		if (i != 57)
